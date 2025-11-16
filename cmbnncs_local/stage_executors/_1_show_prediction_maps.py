21fafca7fa77e742ea9c7bfce15ae405ed5f96e4
JamesAmato <116766236+JamesAmato@users.noreply.github.com>
2025-11-15 20:02:52 -0600

    bugfix to use correct executor

--- cmbnncs_local/stage_executors/_1_show_prediction_maps.py
+++ cmbnncs_local/stage_executors/_1_show_prediction_maps.py
@@ -1,9 +1,10 @@
 from omegaconf import DictConfig
-from cmbml.analysis import CommonRealPostExecutor
+from cmbml.analysis import ShowSimsPostExecutor
 
 
-class cmbNNCSShowPostExecutor(CommonRealPostExecutor):
+class cmbNNCSShowPostExecutor(ShowSimsPostExecutor):
     def __init__(self, cfg: DictConfig) -> None:
-        super().__init__(cfg)
+        stage_str = "show_cmb_post_masked"
+        super().__init__(cfg, stage_str)
         self.right_subplot_title = "cmbNNCS Predicted"
         self.suptitle = cfg.fig_model_name
